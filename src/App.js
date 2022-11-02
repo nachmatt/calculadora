8a02874e8e1c1e5f65024d5905f036102d6d2666
nachmatt <nmatonte@gmail.com>
2022-11-01 21:23:29 -0300

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='body'>
+      
+      <div className='calculadora'>
+
+        <div className="display">
+          <h2 className="historial_display">0</h2>
+          <h2 className="numero_display">0</h2>
+        </div>
+
+        <div className='botones'>
+          <button className="operadores naranja">C</button>
+          <button className="operadores">%</button>
+          <button className="operadores">DEL</button>
+          <button className="operadores">/</button>
+        
+          <button className="numeros">7</button>
+          <button className="numeros">8</button>
+          <button className="numeros">9</button>
+          <button className="operadores">*</button>
+        
+          <button className="numeros">4</button>
+          <button className="numeros">5</button>
+          <button className="numeros">6</button>
+          <button className="operadores">-</button>
+        
+          <button className="numeros">1</button>
+          <button className="numeros">2</button>
+          <button className="numeros">3</button>
+          <button className="operadores">+</button>
+        
+          <button className="operadores">.</button>
+          <button className="numeros">0</button>
+          <button className="operadores naranja col2">=</button>
+        </div>
+
+      </div>
+      
     </div>
   );
 }
